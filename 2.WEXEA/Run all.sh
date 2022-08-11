dc865b4f1b4f6ee0aa4feac9017fa2252cc27425
Hiroaki Takeda <henry_hiroaki@hotmail.com>
2022-08-10 22:02:03 -0700

    first commit

--- 2.WEXEA/Run all.sh
+++ 2.WEXEA/Run all.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 echo "Step 1/7"
-#python 1.title2Id_redirect_parser.py
+python 1.title2Id_redirect_parser.py
 echo "Step 2/7"
-#python 2.article_parser_1.py
+python 2.article_parser_1.py
 echo "Step 3/7"
-#python 3.dicts_creator.py
+python 3.dicts_creator.py
 echo "Step 4/7"
-#python 4.article_parser_2.py
+python 4.article_parser_2.py
 echo "Step 5/7"
-#python 5.article_parser_3_basic.py
+python 5.article_parser_3_basic.py
 echo "Step 6/7"
 python 6.article_extractor_1.py
 echo "Step 7/7"
