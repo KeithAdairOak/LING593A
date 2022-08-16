4f3937fa43ecd6de5278e038c61fd0be83d19dba
Hiroaki Takeda <henry_hiroaki@hotmail.com>
2022-08-16 09:12:23 -0700

    first commit

--- 3.spaCy/2.Train model/train-all.sh
+++ 3.spaCy/2.Train model/train-all.sh
@@ -1,5 +1,4 @@
 python -m spacy train noref.cfg --output ./output_noref --paths.train ./train --paths.dev ./dev --gpu-id 0
-python -m spacy train noref-spacy.cfg --output ./output_noref_spacy --paths.train ./train --paths.dev ./dev 
 python -m spacy train noref-trf.cfg --output ./output_noref_trf --paths.train ./train --paths.dev ./dev --gpu-id 0
 python -m spacy train spacy.cfg --output ./output_from_spacy --paths.train ./train --paths.dev ./dev 
 python -m spacy train spacy-trf.cfg --output ./output_from_spacy_trf --paths.train ./train --paths.dev ./dev
