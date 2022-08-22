2641935f6c43717f642fa2667363298576d11aad
Hiroaki Takeda <henry_hiroaki@hotmail.com>
2022-08-21 22:52:50 -0700

    first commit

--- 3.spaCy/1.Preprocess/Preprocess.sh
+++ 3.spaCy/1.Preprocess/Preprocess.sh
@@ -1,6 +1,6 @@
 # 
-# python preprocess.py [numeric divider] [.json file name which you need to convert to .spacy] 
+# python preprocess.py [numeric divider] [.json file name which you need to convert to .spacy]
 #
-python preprocess.py 10 extracted_wikidataonly_train.json
+python preprocess.py 20 extracted_wikidataonly_train.json
 
 cmd /k
